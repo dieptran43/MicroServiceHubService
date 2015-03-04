d7b7aceb58d9acc09cfe74226521d2fee1b46cf7
Christo Crampton <info@38.co.za>
2015-03-04 14:20:13 +0200

    Add some basic tetst

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.conf import settings
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from tokenauth.authbackends import TokenAuthBackend
+
+import requests 
+import responses
+
+
+def mock_auth_success():
+
+	url = '{0}/users/me/' . format(settings.USER_SERVICE_BASE_URL)		
+	response_string = '{"username": "TEST"}'
+	responses.add(responses.GET, url,
+              body=response_string, status=200,
+              content_type='application/json')
+
+def mock_auth_failure():
+
+	url = '{0}/users/me/' . format(settings.USER_SERVICE_BASE_URL)		
+	responses.add(responses.GET, url,
+              body='', status=401,
+              content_type='application/json')
+
+class ProjectEndpointTestCase(TestCase):
+
+	def setUp(self):
+		self.c = Client(Authorization='Token 123')
+
+	@responses.activate
+	def test_get_microservices_list_requires_auth(self):
+
+		mock_auth_failure()
+		response = self.c.get("/api/v1/microservices/")
+		
+		assert response.status_code == 403, 'Expect permission denied'
+
+	@responses.activate
+	def test_get_microservices_list(self):
+
+		mock_auth_success()
+
+		response = self.c.get("/api/v1/microservices/")
+
+		assert response.status_code == 200, 'Expect 200 OK'
